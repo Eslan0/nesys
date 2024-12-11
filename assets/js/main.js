64e0c9b30fe6d155479f16a12d13982739a83fcb
Eslan069 <sailelog.io@gmail.com>
2024-12-10 22:13:04 -0300

    Add components dynamics in JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,29 @@
-console.clear();
-/*
-const cardsContainer = document.querySelector(".cards");
-const cardsContainerInner = document.querySelector(".cards__inner");
-const cards = Array.from(document.querySelectorAll(".card"));
-const overlay = document.querySelector(".overlay");
+async function loadCard() {
+  try {
+    const response = await fetch('components/html/card.html');
 
-const applyOverlayMask = (e) => {
-  const overlayEl = e.currentTarget;
-  const x = e.pageX - cardsContainer.offsetLeft;
-  const y = e.pageY - cardsContainer.offsetTop;
+    if (!response.ok) throw new Error('Failed to load card');
 
-  overlayEl.style = `--opacity: 1; --x: ${x}px; --y:${y}px;`;
-};
+    const cardHtml = await response.text();
+    
+    document.querySelector('.card').innerHTML = cardHtml;
+  } catch (error) {
+    console.error('Error loading the card:', error);
+  }
+}
+loadCard();
 
-const createOverlayCta = (overlayCard, ctaEl) => {
-  const overlayCta = document.createElement("div");
-  overlayCta.classList.add("cta");
-  overlayCta.textContent = ctaEl.textContent;
-  overlayCta.setAttribute("aria-hidden", true);
-  overlayCard.append(overlayCta);
-};
+async function loadCardEmphas() {
+  try {
+    const response = await fetch('components/html/card_emphas.html');
 
-const observer = new ResizeObserver((entries) => {
-  entries.forEach((entry) => {
-    const cardIndex = cards.indexOf(entry.target);
-    let width = entry.borderBoxSize[0].inlineSize;
-    let height = entry.borderBoxSize[0].blockSize;
+    if (!response.ok) throw new Error('Failed to load card');
 
-    if (cardIndex >= 0) {
-      overlay.children[cardIndex].style.width = `${width}px`;
-      overlay.children[cardIndex].style.height = `${height}px`;
-    }
-  });
-});
-
-const initOverlayCard = (cardEl) => {
-  const overlayCard = document.createElement("div");
-  overlayCard.classList.add("card");
-  createOverlayCta(overlayCard, cardEl.lastElementChild);
-  overlay.append(overlayCard);
-  observer.observe(cardEl);
-};
-
-cards.forEach(initOverlayCard);
-document.body.addEventListener("pointermove", applyOverlayMask);
-*/
+    const cardHtml = await response.text();
+    
+    document.querySelector('.cardEmphas').innerHTML = cardHtml;
+  } catch (error) {
+    console.error('Error loading the card:', error);
+  }
+}
+loadCardEmphas();
